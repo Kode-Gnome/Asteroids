fe842812fd112b996fb09eadf972d5b864da24dd
Kode-Gnome <dgay@LALAFOF1ENG104.kadn.local>
2025-08-06 14:42:20 -0500

    added circles

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -11,8 +13,12 @@ def main():
 
         screen.fill("black")
 
+        clock.tick(60)
         pygame.display.flip()
 
+
+        dt = clock.tick(60)/1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
